ad0f5fd5b2a05fb48e8673caf0af0db7a00a03d2
Guillaume-Lostec <65834941+Guillaume-Lostec@users.noreply.github.com>
2024-03-19 15:32:32 -0600

    Update Modified_Macro.py

--- Modified_Macro.py
+++ Modified_Macro.py
@@ -9,7 +9,7 @@ import xyPlot
 import displayGroupOdbToolset as dgo
 for fr in range(0,101):
     session.viewports['Viewport: 1'].odbDisplay.setFrame(step=0, frame=fr)
-    odb = session.odbs['C:/Users/Guillaume/OneDrive - UCB-O365/Documents/Mullins_idea_March_2024/Coarse/Job-1.odb']
+    odb = session.odbs['path_where_your_job_is/Job-1.odb']
     session.writeFieldReport(fileName='Report_Files/'+str(fr)+'.rpt', append=ON, 
         sortItem='Node Label', odb=odb, step=0, frame=fr, outputPosition=NODAL, 
         variable=(('DMENER', INTEGRATION_POINT), ('SENER', INTEGRATION_POINT), 
