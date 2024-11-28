04de73ed84852922d14dcbe45cc25bf8f830a166
Guillaume Lostec <65834941+Guillaume-Lostec@users.noreply.github.com>
2024-11-27 17:24:46 -0700

    Modified_Macro.py

--- Modified_Macro.py
+++ Modified_Macro.py
@@ -8,7 +8,7 @@ import xyPlot
 import displayGroupOdbToolset as dgo
 for fr in range(0,101):
     session.viewports['Viewport: 1'].odbDisplay.setFrame(step=0, frame=fr)
-    odb = session.odbs['C:\Users\Guillaume\OneDrive - UCB-O365\Documents\Research\Mullins_Damage_Fracture\Github_example\Job-1.odb']
+    odb = session.odbs['Replace_by_relevant_path\Job-1.odb']
     session.writeFieldReport(fileName=str(fr)+'.rpt', append=ON, 
         sortItem='Node Label', odb=odb, step=0, frame=fr, outputPosition=NODAL, 
         variable=(('LE', INTEGRATION_POINT, ((COMPONENT, 'LE11'), (COMPONENT, 
